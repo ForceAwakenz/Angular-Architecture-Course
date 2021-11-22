e9196566cbdcaaa9945464da042ecd2d036d254d
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-22 19:03:02 +0200

    install needed modules and add them into the app.module, set up environment settings for Firebase

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
     production: false,
-    name: 'dev'
-  };
+    name: 'dev',
+    firebase: {
+        config: {
+            apiKey: "AIzaSyDdHwep90wwPkaJiijRPv8cTSlDwP7uYPc",
+            authDomain: "courseapp-8d747.firebaseapp.com",
+            projectId: "courseapp-8d747",
+            storageBucket: "courseapp-8d747.appspot.com",
+            messagingSenderId: "326258671584",
+            appId: "1:326258671584:web:4a89be339ac21074ad3585"
+        }
+    }
+};
