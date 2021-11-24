b4ad8a95c24e24c884e5b897a9085211318998b8
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-24 12:24:53 +0200

    add almost all components, modules, templates for work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', 
+    children: [
+      { 
+      path: 'demo', 
+      loadChildren: () => import('./pages/demo/demo.module').then(m => m.DemoModule) 
+      }
+    ]
+  }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
